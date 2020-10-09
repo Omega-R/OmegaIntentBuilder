99880aac11c9012f71e364d1033b054783ea0e23
just-d-a <prospeedcuber@gmail.com>
2020-10-09 16:09:38 +0300

    add timer button

--- examples/src/main/java/com/omega_r/omegaintentbuilder/MainActivity.java
+++ examples/src/main/java/com/omega_r/omegaintentbuilder/MainActivity.java
@@ -42,6 +42,7 @@ public class MainActivity extends BaseActivity implements View.OnClickListener {
         findViewById(R.id.button_fragment_extra).setOnClickListener(this);
         findViewById(R.id.button_insert_contact).setOnClickListener(this);
         findViewById(R.id.button_search_web).setOnClickListener(this);
+        findViewById(R.id.button_create_timer).setOnClickListener(this);
     }
 
     @Override
@@ -100,6 +101,9 @@ public class MainActivity extends BaseActivity implements View.OnClickListener {
                 break;
             case R.id.button_search_web:
                 onSearchWebClicked();
+                break;
+            case R.id.button_create_timer:
+                onCreateTimerClicked();
                 break;
         }
     }
