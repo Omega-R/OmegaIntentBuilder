e05c7af5173ca29e0e4f18a314a945b6b17a4df1
Anton Knyazev <anton.knyazev@omega-r.com>
2022-04-25 12:28:45 +0300

    Add method web with type Text in intent builder

--- core/src/main/java/com/omega_r/libs/omegaintentbuilder/builders/BrowserBuilder.kt
+++ core/src/main/java/com/omega_r/libs/omegaintentbuilder/builders/BrowserBuilder.kt
@@ -13,16 +13,20 @@ package com.omega_r.libs.omegaintentbuilder.builders
 import android.content.Context
 import android.content.Intent
 import android.net.Uri
+import com.omega_r.libs.omegatypes.Text
+import com.omega_r.libs.omegatypes.toText
 
 /**
  * BrowserBuilder class for starting web browser
  */
-class BrowserBuilder (private val uri: Uri) : BaseActivityBuilder() {
+class BrowserBuilder (private val uri: Text) : BaseActivityBuilder() {
 
-    constructor(urlAddress: String) : this(Uri.parse(urlAddress))
+    constructor(urlAddress: String) : this(urlAddress.toText())
+
+    constructor(uri: Uri) : this(uri.toString())
 
     override fun createIntent(context: Context) =  Intent(Intent.ACTION_VIEW).apply {
-        data = uri
+        data = Uri.parse(uri.getString(context))
     }
 
 }
